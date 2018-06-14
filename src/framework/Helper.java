98904aef97f1b567a9e72a5d67a8e6a9e9a24086
Stefan <stefan.babic@gmail.com>
2018-06-14 20:26:12 +0200

    Learning selenium

---
+++ src/framework/Helper.java
@@ -0,0 +1,19 @@
+package framework;
+
+public class Helper {
+
+    public static String getRandomText() {
+
+//        int randomNumber = (int) (Math.random() * 1000);
+//        String randomText = "sp-" + randomNumber;
+        //sp-32432
+
+//        return randomText;
+        
+        return "sp-" + (int) (Math.random() * 1000);
+        
+        
+        
+    }
+
+}
